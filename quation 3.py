1137992ac4b43d4e3eaaea1ce1186fb465221ac4
Simran8800 <95364621+Simran8800@users.noreply.github.com>
2021-12-09 22:50:25 +0530

    Delete quation 3.py

--- quation 3.py
+++
@@ -1,8 +0,0 @@
-# Check whether 1000 is equal to 1000. If yes, print "barabar hai", else print "barabar nahi hai". 
-# Complete the if statement as well as the print statements in this question.
-# a=1000
-# b=1000
-# if a==b:
-#     print("barabar hai")
-# else:
-#     print("barabar nahi hai")
