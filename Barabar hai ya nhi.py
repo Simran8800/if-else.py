7290ec2690ad800a7c831258677733f9d1223060
Simran8800 <95364621+Simran8800@users.noreply.github.com>
2021-12-10 09:09:26 +0530

    Add files via upload

---
+++ Barabar hai ya nhi.py
@@ -0,0 +1,11 @@
+# Create a flowchart that checks whether the result of subtracting 33 from 100 is equal 
+# to 55 Download the flowchart file from draw.io and submit here.
+
+a=100-33
+b=55
+if a==b:
+       print(a,"barabar hai")
+else:
+       print(a,"....",b,"ke brabar nahi hai")
+
+       
