f715df4f30f430de11d59a307cb9b2f1203928b2
Simran8800 <95364621+Simran8800@users.noreply.github.com>
2021-12-10 09:38:00 +0530

    Add files via upload

---
+++ Equal & not equal.py
@@ -0,0 +1,8 @@
+# Check whether 1000 is greater than or equal to 4000. If yes, print "barabar ya bada hai".
+#  Else print "nahi hai". Complete the print statements in this question.
+var=int(input("enter the number"))
+if var==1000:
+       print("barabar hai")
+else:
+   print("barabar nahi hai")
+
