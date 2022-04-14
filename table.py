29ce9f53e49c4b905848d27482d0c18bf6bb02de
simran <95364621+Simran8800@users.noreply.github.com>
2022-04-14 15:52:17 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,28 @@
+# a=int(input("enter the no"))
+# if a%10:
+#     print("\n",a*1,'*',1,"=",a*1,"\n",a*2,'*',2,"=",a*2,"\n",a*3,'*',3,"=",a*3,"\n",a*4,'*',4,"=",a*4,"\n",a*5,'*',5,"=",a*5,"\n",a*6,'*',6,"=",a*6,"\n",a*7,'*',7,"=",a*7,"\n",a*8,'*',8,"=",a*8,"\n",a*9,'*',9,"=",a*9,"\n",a*10,'*',10,"=",a*10,)
+# else:
+#     ("wrong")
+    
+    
+# num=int(input("enter the number"))
+# n=int(input("enter the number"))
+# while num<=n:
+#     i=1
+#     while i<=10:
+#         print(num*i,end=' ')
+#         i=i+1
+#     print()
+#     num=num+1
+
+a=int(input("enter the no"))
+i=0
+while i<=a:
+    j=0
+    while j<=10:
+        print(i,"*",j,"=",j*1)
+        j=j+1
+    print()
+    i=i+1
+    
+    
